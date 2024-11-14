32ff2a8b7258f1b20f048489bdb52aa8c5bf057f
hellijah <thierry.corlieto@gmail.com>
2024-11-14 15:04:41 +0100

    ajout autre version script 3

--- script_3.js
+++ script_3.js
@@ -2,3 +2,36 @@ let etages = prompt("Salut, bienvenue dans ma super pyramide ! Combien d'étages
 for (let i = 1; i <= etages; i++) {
   console.log(" ".repeat(etages - i) + "#".repeat(i));
 }
+
+//autre version
+
+/*
+// Demander à l'utilisateur le nombre d'étages pour la pyramide
+const etages = parseInt(prompt("Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"));
+//parseInt conversion en entier 
+
+// Vérifie si l'entrée est bien un nombre et positif
+if (isNaN(etages) || etages <= 0) { // isNaN (signifiant "is Not-a-Number") renvoie true si la valeur de etages n’est pas un nombre
+    console.log("Merci d'entrer un nombre valide et positif.");
+    return;
+}
+
+// Boucle principale pour chaque étage
+for (let i = 1; i <= etages; i++) {
+    let ligne = "";
+
+    // Première boucle pour ajouter les espaces
+    for (let j = 0; j < etages - i; j++) {
+        ligne += " ";
+    }
+
+    // Deuxième boucle pour ajouter les #
+    for (let k = 0; k < i; k++) {
+        ligne += "#";
+    }
+
+    // Affiche la ligne complète de la pyramide dans la console
+    console.log(ligne);
+}
+}
+*/
--- script_7.js
+++ script_7.js
@@ -1,9 +1,9 @@
-// Fonction pour gérer la conversation avec le bot
+
 function acneBot() {
-  // Demande à l'utilisateur d'entrer une phrase
+
   let userMessage = prompt("Parle au bot adolescent :");
 
-  // Vérifie la phrase selon les règles établies
+  // Vérifie la phrase selon les condiitons demandées
   if (userMessage.endsWith("?")) {
     console.log("Ouais Ouais...");
   } else if (userMessage === userMessage.toUpperCase() && userMessage !== "") {
